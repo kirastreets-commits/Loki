caa54786554f8f80f06b0a0e877a7aaa38f75478
kirastreets-commits <kirastreets@hotmail.com>
2026-06-21 23:21:32 +1000

    Update quin.py

--- commands/quin.py
+++ commands/quin.py
@@ -44,7 +44,7 @@ aquiny = [
     "clicking things he shouldn’t (again)",
     "getting distracted by glowing interfaces",
     "organizing data “based on vibes” instead of logic",
-    "trying to sing at a computer to 'make it work faster' (helping Ender)”,
+    "trying to sing at a computer to make it work faster \(he's helping Ender\)”,
     "sitting quietly after overstimulation",
     "missing someone but not saying it",
     "replaying memories through threadsong",
