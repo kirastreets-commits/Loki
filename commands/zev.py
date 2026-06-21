a8fa3d58be4fe460a6414ca82128daaa86850747
kirastreets-commits <kirastreets@hotmail.com>
2026-06-21 20:46:01 +1000

    Update zev.py

--- commands/zev.py
+++ commands/zev.py
@@ -6,7 +6,7 @@ zevryn = [
     "polishing and cleaning his weapons",
     "training in his training room",
     "going through a slow set of martial arts moves",
-    "sitting cross-legged, his eyes closed, apparently meditating on something" 
+    "sitting cross-legged, his eyes closed, apparently meditating on something", 
     "discussing plans with the gang",
     "having a private chat with Silk",
     "out on a job to save some magical creatures",
