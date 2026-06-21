0afaac951b14183883ff3eb0e40494fe6e35f128
kirastreets-commits <kirastreets@hotmail.com>
2026-06-21 20:41:56 +1000

    Add files via upload

---
+++ commands/female_avian.py
@@ -0,0 +1,22 @@
+import random
+
+starts = [
+    "Qu", "K", "T", "S", "V",
+    "L", "R", "W", "N", "C", "E"
+]
+
+middles = [
+    "i", "e", "a", "ae",
+    "ai", "y", "o", "i"
+]
+
+ends = [
+    "a", "ra", "ia", "la",
+    "na", "li", "ri", "i"
+]
+
+def setup(bot):
+    @bot.command()
+    async def female_avian(ctx):
+        result = random.choice(starts) + random.choice(middles) + random.choice(ends)
+        await ctx.send("Your Avian's name is" (result))
