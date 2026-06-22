aac968dbbc3e4e41bea4472130c3dc137272f945
kirastreets-commits <kirastreets@hotmail.com>
2026-06-22 17:11:02 +1000

    Update loki.py

--- loki.py
+++ loki.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-from commands import theft, prompt, dragon, dragonmood, ith, ash, zev, quin, male_avian, female_avian, silk, wmoods
+from commands import theft, prompt, dragon, dragonmood, ith, ash, zev, quin, male_avian, female_avian, silk, wmoods, sevka
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -26,6 +26,7 @@ male_avian.setup(bot)
 female_avian.setup(bot)
 wmoods.setup(bot)
 silk.setup(bot)
+sevka.setup(bot)
 
 import os
 
